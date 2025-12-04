3a99bf3070690b27f4291f3d45cb5ef1814e2c6c
DarkDavet <darkdavet@gmail.com>
2025-12-04 14:59:42 +0300

    [DEV] created and added scene loader

---
+++ Assets/Scripts/global_logic/SceneLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadLevel1Scene()
+    {
+        StartCoroutine(LoadAndStartLevel1());
+    } 
+
+    public void LoadMainMenuScene()
+    {
+        StartCoroutine(LoadAndStartMainMenu());
+    }
+    private IEnumerator LoadAndStartLevel1()
+    {
+        yield return Load(Scenes.LEVEL_1);
+        yield return new WaitForSeconds(2);
+
+    }
+
+    private IEnumerator LoadAndStartMainMenu()
+    {
+        yield return Load(Scenes.MAIN_MENU);
+        yield return new WaitForSeconds(2);
+
+    }
+
+    private IEnumerator Load(string sceneName)
+    {
+        yield return SceneManager.LoadSceneAsync(sceneName);
+    }
+}
---
+++ Assets/Scripts/global_logic/Scenes.cs
@@ -0,0 +1,6 @@
+
+public class Scenes
+{
+    public const string LEVEL_1 = "level_1_scene";
+    public const string MAIN_MENU = "main_menu_scene";
+}
