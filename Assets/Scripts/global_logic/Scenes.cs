a852acb16c301641d06a99438de4b573f948c0e9
DarkDavet <darkdavet@gmail.com>
2025-12-04 15:29:28 +0300

    [TD] added buttons with transition between scenes

--- Assets/Scripts/global_logic/Scenes.cs
+++ Assets/Scripts/global_logic/Scenes.cs
@@ -1,6 +1,6 @@
 
 public class Scenes
 {
-    public const string LEVEL_1 = "level_1_scene";
-    public const string MAIN_MENU = "main_menu_scene";
+    public const string MAIN_MENU = "MainMenu";
+    public const string LEVEL_1 = "Level_1";
 }
