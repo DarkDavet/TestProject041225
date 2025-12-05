c28d0b3ec5dae13be400b1d264a2bbabec1d64df
DarkDavet <darkdavet@gmail.com>
2025-12-05 18:37:07 +0300

    [DEV/TD] optimized projectile prefab

--- Assets/Scripts/local_logic/Projectile.cs
+++ Assets/Scripts/local_logic/Projectile.cs
@@ -1,3 +1,4 @@
+using LazySquirrelLabs.SphereGenerator.Generators;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +7,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private MeshFilter _meshFilter;
+
     public string tagProjectile;
     public float damage = 10;
     public float lifeTime = 3f;
+    
+
+    private void Awake()
+    {
+        SphereGenerator generator = new IcosphereGenerator(1, 0);
+        Mesh mesh = generator.Generate();
+        _meshFilter.mesh = mesh;
+    }
 
     void Start()
     {
